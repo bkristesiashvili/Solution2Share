50ea71da075630083fa720eae9e4aa2a38905edb
Besik Kristesiashvili <besik.kristesiashvili@saatec.com>
2021-07-18 14:34:30 +0400

    Restore some deleted files

---
+++ Solution2Share.Service/GraphService/GraphScopes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution2Share.Service
+{
+    public static class GraphScopes
+    {
+        #region STATIC FIELDS
+
+        public readonly static string[] Scopes =
+        {
+            "User.Read",
+            "Organization.Read.All",
+            "Directory.Read.All",
+            "MailboxSettings.Read",
+            "Calendars.ReadWrite"
+        };
+
+        #endregion
+    }
+}
